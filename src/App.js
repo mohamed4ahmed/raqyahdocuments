d476bc9c76ce1159d7cc0e53fc095e4be1d045c7
unknown <mohamed.ahmed8412000@gmail.com>
2022-11-19 17:59:16 +0300

    changeDossdmaddtddffddfds

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import dareba1 from "./assets/dareba1.png";
+import dareba2 from "./assets/dareba2.png";
+import ro5stbaladya from "./assets/ro5stbaladya.png";
+import ro5stbona from "./assets/ro5stbona.png";
+import ro5stsna3ya1 from "./assets/ro5stsna3ya1.png";
+import sgeltogary2 from "./assets/sgeltogary2.png";
+import shahadas3wada from "./assets/shahadas3wada.png";
+import tasre7be2a from "./assets/tasre7be2a.png";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='documents'> 
+        <img src={dareba1} alt='dareba1'></img>
+        <img src={dareba2} alt='dareba2'></img>
+        <br></br>
+        <img src={ro5stbaladya} alt='ro5stbaladya'></img>
+        <br></br>
+        <img src={ro5stbona} alt='ro5stbona'></img>
+        <br></br>
+        <img src={ro5stsna3ya1} alt='ro5stsna3ya1'></img>
+        <br></br>
+        <img src={sgeltogary2} alt='sgeltogary2'></img>
+        <br></br>
+        <img src={shahadas3wada} alt='shahadas3wada'></img>
+        <br></br>
+        <img src={tasre7be2a} alt='tasre7be2a'></img>
+      </div>
+     
     </div>
   );
 }
